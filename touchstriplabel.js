ca0886b8308c0eca04cffab024281587dc36a558
Ava Hazel <ava@ava>
2023-02-02 04:17:59 -0600

    Updated home page

---
+++ touchstriplabel.js
@@ -0,0 +1,4 @@
+var touchstripLable0 = document.getElementsById("touchstrip-label0");
+var touchstripLable1 = document.getElementsById("touchstrip-label1");
+touchstripLable0.style.display = 'none';
+touchstripLable1.style.display = 'none';
