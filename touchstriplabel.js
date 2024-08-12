5964cf48791d602ca1c77ff72be62b8cbdfcc2f7
Ava Loveless <Ava@vacado76>
2024-08-11 23:46:51 -0500

    Removed old JS and updated font size for Max_Into

--- touchstriplabel.js
+++
@@ -1,4 +0,0 @@
-var touchstripLable0 = document.getElementsById("touchstrip-label0");
-var touchstripLable1 = document.getElementsById("touchstrip-label1");
-touchstripLable0.style.display = 'none';
-touchstripLable1.style.display = 'none';
