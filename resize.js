088087e18be8c69fb0418e6701021a646c3e0bb8
Ava Loveless <Ava@vacado76>
2024-08-16 02:42:13 -0500

    Updated hamburger button

--- resize.js
+++ resize.js
@@ -1,6 +1,6 @@
-var elem = document.getElementById('myCanvas');
+var canvas = document.getElementById('myCanvas');
 var dropdown = document.getElementById('nav-bar-drop-down');
-elem.addEventListener('click', function(event) {display()});
+canvas.addEventListener('click', function(event) {display()});
 
 function display(){
     if(dropdown.hidden){
@@ -9,3 +9,18 @@ function display(){
         dropdown.hidden = true;
     }
 };
+
+var ctx = canvas.getContext("2d");
+ctx.strokeStyle = "#ffffff";
+ctx.lineWidth = 20;
+ctx.moveTo(0,25);
+ctx.lineTo(2000,25);
+ctx.stroke();
+
+ctx.moveTo(0,75);
+ctx.lineTo(2000,75);
+ctx.stroke();
+
+ctx.moveTo(0,125);
+ctx.lineTo(2000,125);
+ctx.stroke();
