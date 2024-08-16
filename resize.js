8718a0cbaf50b699c949087fae38a037a42ddbf1
Ava Loveless <Ava@vacado76>
2024-08-15 23:22:17 -0500

    Updated navbar

--- resize.js
+++ resize.js
@@ -1,7 +1,11 @@
-function showHeight(){
-    console.log(window.screen.availHeight)
-}
+var elem = document.getElementById('myCanvas');
+var dropdown = document.getElementById('nav-bar-drop-down');
+elem.addEventListener('click', function(event) {display()});
 
-function showWidth(){
-    console.log(window.screen.availWidth)
-}
+function display(){
+    if(dropdown.hidden){
+        dropdown.hidden = false;
+    } else {
+        dropdown.hidden = true;
+    }
+};
