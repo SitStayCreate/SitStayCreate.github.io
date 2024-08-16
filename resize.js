8c8296dc4a98ec004e3761c45cce5ee9fa653ee5
Ava Loveless <Ava@vacado76>
2024-08-16 11:48:50 -0500

    Fixed navbar bugs

--- resize.js
+++ resize.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById('myCanvas');
 var dropdown = document.getElementById('nav-bar-drop-down');
 canvas.addEventListener('click', function(event) {display()});
+closedMenu();
 
 function display(){
     if(dropdown.hidden){
@@ -10,17 +11,44 @@ function display(){
     }
 };
 
-var ctx = canvas.getContext("2d");
-ctx.strokeStyle = "#ffffff";
-ctx.lineWidth = 20;
-ctx.moveTo(0,25);
-ctx.lineTo(2000,25);
-ctx.stroke();
 
-ctx.moveTo(0,75);
-ctx.lineTo(2000,75);
-ctx.stroke();
+function closedMenu(){
+    var ctx = canvas.getContext("2d");
+    ctx.strokeStyle = "#ffffff";
+    ctx.lineWidth = 20;
+    ctx.moveTo(0,canvas.height*.2);
+    ctx.lineTo(canvas.width,canvas.height*.2);
+    ctx.stroke();
+    
+    ctx.moveTo(0,canvas.height*.5);
+    ctx.lineTo(canvas.width,canvas.height*.5);
+    ctx.stroke();
+    
+    ctx.moveTo(0,canvas.height*.8);
+    ctx.lineTo(canvas.width,canvas.height*.8);
+    ctx.stroke();
+}
 
-ctx.moveTo(0,125);
-ctx.lineTo(2000,125);
-ctx.stroke();
+// Might not use this
+function openMenu(){
+    var ctx = canvas.getContext("2d");
+    ctx.strokeStyle = "#ffffff";
+    ctx.lineWidth = 20;
+    ctx.moveTo(canvas.width*.2,0);
+    ctx.lineTo(canvas.width*.2,canvas.height);
+    ctx.stroke();
+    
+    ctx.moveTo(canvas.width*.5,0);
+    ctx.lineTo(canvas.width*.5,canvas.height);
+    ctx.stroke();
+    
+    ctx.moveTo(canvas.width*.8,0);
+    ctx.lineTo(canvas.width*.8,canvas.height);
+    ctx.stroke();
+}
+
+// Not working
+function clearCanvas(){
+    var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
